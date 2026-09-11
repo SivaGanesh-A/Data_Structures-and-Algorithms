9ee6b90acde06540420965402107817f9369d6ee
SivaGanesh <sivaganeshakula333@gmail.com>
2026-09-11 20:53:17 +0530

    Time: 25 ms (32.09%), Space: 22.8 MB (68.84%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -2,12 +2,28 @@ import heapq
 
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        
+
         heap = []
-        n = len(matrix)
-        for row in range(n):
-            heapq.heappush(heap, (matrix[row][0], row, 0))
-        for _ in range(k):
+        for row in range(len(matrix)):
+            value = matrix[row][0]
+            heapq.heappush(heap, (value, row, 0))
+        for i in range(k):
             value, row, col = heapq.heappop(heap)
-            if col + 1 < n:
-                heapq.heappush(heap, (matrix[row][col+1], row, col +1))
-        return value
+            next_col = col + 1
+            
+            if next_col < len(matrix[row]):
+                next_val = matrix[row][next_col]
+
+                heapq.heappush(heap, (next_val, row, next_col))
+        return value        
+        
+        # heap = []
+        # n = len(matrix)
+        # for row in range(n):
+        #     heapq.heappush(heap, (matrix[row][0], row, 0))
+        # for _ in range(k):
+        #     value, row, col = heapq.heappop(heap)
+        #     if col + 1 < n:
+        #         heapq.heappush(heap, (matrix[row][col+1], row, col +1))
+        # return value
